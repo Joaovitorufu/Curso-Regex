a1dfb7153cfd2cdd0134476ffa02d56761af51bc
Joaovitorufu <joaovitorufu@gmail.com>
2021-06-16 10:24:39 -0300

    intervalos e ajustes em conjuntos

--- conjuntos/Conjuntos.js
+++ conjuntos/Conjuntos.js
@@ -2,3 +2,7 @@ const texto = ' 1,2,3,4,5,6,a.b c!d?e[f'
 //pra definir uma classe (ou conjunto) de caracteres usa [];
 const regexPares = /[02468]/g
 console.log(texto.match(regexPares)); // no caso vai utilizar do metaCaractere | para procurar globalmente.
+
+const texto2 = 'João não vai passear na moto.'
+const regexComESemAcento = /n[aã]/g
+console.log(texto2.match(regexComESemAcento));
