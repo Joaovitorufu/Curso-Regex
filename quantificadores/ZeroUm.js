d3505a1be945a4778d1dec4608df3405e9987426
Joaovitorufu <joaovitorufu@gmail.com>
2021-06-17 08:37:01 -0300

    introdução quantificadores

---
+++ quantificadores/ZeroUm.js
@@ -0,0 +1,6 @@
+const texto1 = 'De longe eu avistei o fogo e uma pessoa gritando : FOGOOOO!'
+const texto2 = 'There is a big fog in NYC'
+
+const regex = /fogo?/gi
+console.log(texto1.match(regex));
+console.log(texto2.match(regex));
