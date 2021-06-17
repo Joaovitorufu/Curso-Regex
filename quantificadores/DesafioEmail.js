b585e4664c1650f2232cb4471cc8ac5f6ebeed0c
Joaovitorufu <joaovitorufu@gmail.com>
2021-06-17 11:19:31 -0300

    Desafio Email feito corretamente

---
+++ quantificadores/DesafioEmail.js
@@ -0,0 +1,9 @@
+const texto = `
+Os e-mails dos convidados são:
+    - fulano@cod3r.com.br
+    - xico@gmail.com
+    - joao@empresa.info.br
+    -maria_silva@registro.br
+    - rafa.sampaio@yahoo.com
+`
+console.log(texto.match(/\w{1,}@\w{1,}\.\w{1,}\.?\w{1,}/g));
